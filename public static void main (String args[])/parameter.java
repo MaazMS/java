5dc84429b830d0a864dd7a7bd82e8fa56da9ec14
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-18 18:02:56 -0700

    Create parameter.java

---
+++ public static void main (String args[])/parameter.java
@@ -0,0 +1,14 @@
+class hello 
+{
+	public static  void main()
+	{
+		
+	System.out.print("hello world");
+				
+	}
+}
+/*if parameter not written runtime error:
+Error: Main method not found in class hello, please define the main method as:
+   public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+*/

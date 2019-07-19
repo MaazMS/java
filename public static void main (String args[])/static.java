d6f667c62de478d90ec38b1d8aa0b2195e41ae2a
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-18 18:01:53 -0700

    Create static.java

---
+++ public static void main (String args[])/static.java
@@ -0,0 +1,14 @@
+class hello 
+{
+	public static  void main()
+	{
+		
+	System.out.print("hello world");
+				
+	}
+}
+
+/* if static keyword not written runtime error:
+Error: Main method is not static in class hello, please define the main method as:
+public static void main(String[] args)
+*/

e3f91d59ead465ec12c17d8ab97c9c68e358aa04
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-07-18 18:00:29 -0700

    Create public and static .java

---
+++ public static void main (String args[])/public and static .java
@@ -0,0 +1,21 @@
+class hello 
+{
+	public static  void main()
+	{
+		
+	System.out.print("hello world");
+				
+	}
+}
+/* if public keyword not written runtime error:
+ Main method not found in class hello, please define the main method as:
+public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+*/
+//OR 
+
+/*if public, static keyword not written runtime error:
+Error: Main method not found in class hello, please define the main method as:
+   public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+*/

b7432b8c5bdab4fd711ad91b515cd4f0fa5a7b78
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-10-03 20:54:09 -0700

    Update websitePro.java

--- Method Overriding/websitePro.java
+++ Method Overriding/websitePro.java
@@ -2,58 +2,45 @@ import java.io.*;
 
 public class website
 {
-
-	public void function()
-	{
-  
-		System.out.println("This is base class function");
-	
+  public void function()
+  {
+      System.out.println("This is base class function");
   }
   
-  
-	public void display()         // This method is override in driver class
-	{
-  
-		System.out.println("This is base class");
+   public void display()         // This method is override in driver class
+   {
+       System.out.println("This is base class");
+   }
 	
-  }
-	
-}
+ }
 
 public class websitePro extends website
-	{
-	
+{
   public void method()
-	{
-  
-		System.out.println("This is drived class method");
-	
+  {
+      System.out.println("This is drived class method");
   }
   
-		public void display()  // This method is override method 
-		{
-    
-			System.out.println("This is drived class");
-		
+  public void display()  // This method is override method 
+   {
+    	System.out.println("This is drived class");
     }
 		
     public static void main(String args[])
-		{
-    
-			websitePro object1 = new websitePro();
-			object1.display();
-			
-      website object2 = new website();
-			object2.display(); 
-			
-      website object3 = new websitePro(); 
-			object3.display();
-			object3.function(); 
-			//object3.method();  error The method method() is undefined for the type website
-
-		
-    }
-	}
+    {
+        websitePro object1 = new websitePro();  // Drived class object call drived class method
+	  object1.display();
+	 
+	 website object2 = new website();       // base class object call base class method
+	 object2.display(); 
+	 
+	 website object3 = new websitePro();    //This object call only overriden method and not call to any normal method of drived class. 
+	 object3.display();
+	 object3.function(); 
+	 //object3.method();  error The method method() is undefined for the type website
+      
+      }
+   }
 
 /*OUTPUT
 This is drived class

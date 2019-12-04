09812ec9a3bf6b562d51208a2e646efe79c2e63d
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-12-04 08:16:04 -0800

    Create integer.java

---
+++ Input and Output/Scanner/integer.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class integer
+{
+	public static void main(String args[])
+	{
+		int number;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the integer number ");
+		number= sc.nextInt();
+		System.out.println(number);
+	}
+}
+/*
+Enter the number 
+5
+5
+*/

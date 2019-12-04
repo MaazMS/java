f758dfe7f105a1bbf38df47a2aa5f2bcdd44c55b
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2019-12-04 08:18:47 -0800

    Create name.java

---
+++ Input and Output/Scanner/name.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class name
+{
+	public static void main(String args[])
+	{
+		String first,last;
+		Scanner Sc= new Scanner(System.in);
+		
+		System.out.println("Enter the first name");
+		first = Sc.nextLine();
+		
+		System.out.println("Enter the last name");
+		last = Sc.nextLine(); 
+		
+		System.out.println("My name is "+ first +" "+ last);			
+	}
+}
+/*
+Enter the first name
+maaz 
+Enter the last name
+shaikh
+My name is maaz shaikh
+*/

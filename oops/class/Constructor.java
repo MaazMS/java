3b5f23a9dd7a1fd828a6578cd15a963b06894887
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-05-28 09:34:53 -0700

    default Constructor create by compiler

---
+++ oops/class/Constructor.java
@@ -0,0 +1,17 @@
+
+public class Constructor {
+	
+	int number; 
+	String name; 
+	
+	public static void main(String args[]) {
+		
+		Constructor object = new Constructor();
+		System.out.println("default vale of number	"+object.number);
+		System.out.println("default vale of name	"+object.name);
+	}
+}
+/*
+ * default vale of number	0
+ * default vale of name	null
+ */

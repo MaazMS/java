4e8f6ba6bcd5fc2d05927207440a2d631cba9e97
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-05-28 09:50:40 -0700

    Parameter Constructor

---
+++ oops/class/ParameterConstructor.java
@@ -0,0 +1,17 @@
+public class ParameterConstructor {
+
+	int number;
+	String name;
+	
+	public ParameterConstructor(int number,String name) {
+		this.number=number;
+		this.name=name;
+	}
+	
+	public static void main(String args[]) {
+		
+		ParameterConstructor object = new ParameterConstructor(101,"Maaz");
+		System.out.println("default vale of number	" + object.number);
+		System.out.println("default vale of name	" + object.name);
+	}
+}

dd5393be043a9f016377c72c75a3964de3b41b17
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-05-30 11:08:53 -0700

    Developer class (intermediate) is extends Employee class.

---
+++ oops/Inheritance/multiLevel/Developer.java
@@ -0,0 +1,10 @@
+package multiLevel;
+
+public class Developer extends Employee { 
+	
+	public int profile() {
+		System.out.println("Which prfile you work web,android,open source etc");
+		return 0;
+	}
+	
+}

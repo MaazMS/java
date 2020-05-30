99ace3a5fbac84479279d0abe8ce5edd1b947946
Maazil3861 <34999445+Maazil3861@users.noreply.github.com>
2020-05-30 11:10:46 -0700

    WebDeveloper class is extends Developer class (intermediate)

---
+++ oops/Inheritance/multiLevel/WebDeveloper.java
@@ -0,0 +1,10 @@
+package multiLevel;
+
+public class WebDeveloper extends Developer {
+	
+	public static void main(String args[]) {
+		
+		WebDeveloper object = new WebDeveloper(); 
+		object.profile();
+	}
+}
